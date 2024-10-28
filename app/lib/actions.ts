c507c16e1419013174fa99d711d7c3da0e4527fc
lgorsk923 <118467623+lgorsk923@users.noreply.github.com>
2024-10-28 12:03:49 -0500

    activation of search element for invoice page, and beginning of server Actions

---
+++ app/lib/actions.ts
@@ -0,0 +1,10 @@
+'use server';
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerID: formData.get('customerID'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+    console.log(rawFormData)
+}
